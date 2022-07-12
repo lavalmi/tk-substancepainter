7788d1c2681037ca694f72a987e3429f00fba251
JanPhKoch <janphkoch@gmail.com>
2022-07-12 11:39:29 +0200

    changed MINIMUM_SUPPORTED_VERSION to 7.4.3

--- engine.py
+++ engine.py
@@ -37,7 +37,7 @@ __contact__ = "https://www.linkedin.com/in/diegogh/"
 # when Substance Painter software version is above the tested one.
 SHOW_COMP_DLG = "SGTK_COMPATIBILITY_DIALOG_SHOWN"
 
-MINIMUM_SUPPORTED_VERSION = "2018.3"
+MINIMUM_SUPPORTED_VERSION = "7.4.3"
 
 
 def to_new_version_system(version):
